a48737605bda1d4a55faa0329b8540056977983e
JohanvanAlphen <74180158+JohanvanAlphen@users.noreply.github.com>
2021-01-08 10:14:55 +0100

    Add files via upload

---
+++ Opdracht9_For_Loops/multiplication-tables.js
@@ -0,0 +1,10 @@
+// for (i = 1; i <= 10; i++) {
+
+//     let number = 9;
+//     console.log(i + " x " + number + " = " + (i * number));
+// }
+for (i = 1; i <= 10; i++) {
+
+    for (x = 1; x <= 10; x++)
+        console.log(x + " x " + i + " = " + (i * x));
+}
